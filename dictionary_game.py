0cb3a10a1aab7def2f99b134a052cd320470bc90
hiteshsandal <hitesh.sandal5@gmail.com>
2017-12-23 00:22:14 +0530

    Add files via upload

---
+++ dictionary_game.py
@@ -0,0 +1,67 @@
+# do this by creating two program, cave_initialise.py should create the two
+#shelves(locations and vocabulary) with the appropriate keys and values.
+#
+# cave_game.py will then use the two shelves instead of dictionaries.
+#Apart from opening and closing the shelves, cave_game will need only
+# two changes to the actual code - remember that shelf jets MUST be strings!
+
+# just to be clear, cave_game.py will contain the code from line 45, everything
+# befire that(modified to use shelves) will be in cave_initialise.py
+locations = {0: {"desc": "You are sitting in front of a computer learning Python",
+                 "exits": {},
+                 "namedExits": {}},
+             1: {"desc": "You are standing at the end of a road before a small brick building",
+                 "exits": {"W": 2, "E": 3, "N": 5, "S": 4, "Q": 0},
+                 "namedExits": {"2": 2, "3": 3, "5": 5, "4": 4}},
+             2: {"desc": "You are at the top of a hill",
+                 "exits": {"N": 5, "Q": 0},
+                 "namedExits": {"5": 5}},
+             3: {"desc": "You are inside a building, a well house for a small stream",
+                 "exits": {"W": 1, "Q": 0},
+                 "namedExits": {"1": 1}},
+             4: {"desc": "You are in a valley beside a stream",
+                 "exits": {"N": 1, "W": 2, "Q": 0},
+                 "namedExits": {"1": 1, "2": 2}},
+             5: {"desc": "You are in the forest",
+                 "exits": {"W": 2, "S": 1, "Q": 0},
+                 "namedExits": {"2": 2, "1": 1}}
+             }
+
+vocabulary = {"Quit": "Q",
+              "NORTH": "N",
+              "SOUTH": "S",
+              "EAST": "E",
+              "WEST": "W",
+              "ROAD": "1",
+              "HILL": "2",
+              "BUILDING": "3",
+              "VALLEY": "4",
+              "FOREST": "5"}
+
+loc = 1
+while True:
+    availableExits = ", ".join(exits[loc].keys())
+
+    print(locations[loc])
+
+    if loc == 0:
+        break
+    else:
+        allExits = exits[loc].copy()
+        allExits.update(namedExits[loc])
+
+    direction = input("Available exits are " + availableExits).upper()
+    print()
+
+    # Parse the user input, using our vocabulary dictionary if necessary
+    if len(direction) > 1:  # more than 1 letter, so check vocab
+        words = direction.split()
+        for word in words:
+            if word in vocabulary:   # does it contain a word we know?
+                direction = vocabulary[word]
+                break
+
+    if direction in allExits:
+        loc = allExits[direction]
+    else:
+        print("You cannot go in that direction")
